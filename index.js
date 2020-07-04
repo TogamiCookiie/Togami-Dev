ae38cfdc22953dd373cacf89e7bf346d32a431a3
Damian Freeman <38473158+TogamiCookiie@users.noreply.github.com>
2020-07-04 07:36:49 -0400

    kek

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const { Client } = require("discord.js");
+const { Client, Guild } = require("discord.js");
 const client = new Client();
 
 client.once("ready", () => {
     console.log(`Logged in as ${client.user.username}#${client.user.discriminator} and on ${client.guilds.cache.size} guilds.`);
+});
+
+client.on("message", (msg) => {
+    if (msg.author.bot) return;
+
+    if (msg.content === "ping") {
+        return msg.reply("pong!");
+    }
 });
 
 client.login(require("./config.json").token);
